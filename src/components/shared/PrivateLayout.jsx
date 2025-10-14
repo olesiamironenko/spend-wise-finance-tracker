047e200fc165c1bcaeb32f14df98e162c5ed99d2
Olesia <136620028+olesiamironenko@users.noreply.github.com>
2025-10-13 23:20:50 -0400

    Update src/components/shared/PrivateLayout.jsx

--- src/components/shared/PrivateLayout.jsx
+++ src/components/shared/PrivateLayout.jsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import Layout from './Layout';
 
 export default function PrivateLayout({ children }) {
-  const isAuthenticated = localStorage.getItem('user'); // or useContext(AuthContext)
+  const isAuthenticated = localStorage.getItem('user'); 
 
   if (!isAuthenticated) return <Navigate to="/login" replace />;
   return <Layout>{children}</Layout>;
